af2310960c2255944bb234e3792aebacb6055e55
Keyur Savaliya <keyurs8589@gmail.com>
2020-09-23 15:43:17 +1000

    ADD: corporate Management routers and components

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -1,43 +1,48 @@
-import React, { Suspense, lazy } from "react";
-import { Redirect, Switch, Route } from "react-router-dom";
-import { LayoutSplashScreen, ContentRoute } from "../_metronic/layout";
-import { BuilderPage } from "./pages/BuilderPage";
-import { MyPage } from "./pages/MyPage";
-import { DashboardPage } from "./pages/DashboardPage";
+import React, { Suspense, lazy } from 'react';
+import { Redirect, Switch, Route } from 'react-router-dom';
+import { LayoutSplashScreen, ContentRoute } from '../_metronic/layout';
+import { BuilderPage } from './pages/BuilderPage';
+import { MyPage } from './pages/MyPage';
+import { DashboardPage } from './pages/DashboardPage';
 import AdminManagement from './pages/AdminManagement';
+import CorporateManagement from './pages/CorporateManagement';
 
 const GoogleMaterialPage = lazy(() =>
-	import("./modules/GoogleMaterialExamples/GoogleMaterialPage")
+  import('./modules/GoogleMaterialExamples/GoogleMaterialPage')
 );
 const ReactBootstrapPage = lazy(() =>
-	import("./modules/ReactBootstrapExamples/ReactBootstrapPage")
+  import('./modules/ReactBootstrapExamples/ReactBootstrapPage')
 );
 const ECommercePage = lazy(() =>
-	import("./modules/ECommerce/pages/eCommercePage")
+  import('./modules/ECommerce/pages/eCommercePage')
 );
 
 export default function BasePage() {
-	// useEffect(() => {
-	//   console.log('Base page');
-	// }, []) // [] - is required if you need only one call
-	// https://reactjs.org/docs/hooks-reference.html#useeffect
+  // useEffect(() => {
+  //   console.log('Base page');
+  // }, []) // [] - is required if you need only one call
+  // https://reactjs.org/docs/hooks-reference.html#useeffect
 
-	return (
-		<Suspense fallback={<LayoutSplashScreen />}>
-			<Switch>
-				{
-					/* Redirect from root URL to /dashboard. */
-					<Redirect exact from="/" to="/dashboard" />
-				}
-				<ContentRoute path="/dashboard" component={DashboardPage} />
-				<ContentRoute path="/admin-management" component={AdminManagement} />
-				<ContentRoute path="/builder" component={BuilderPage} />
-				<ContentRoute path="/my-page" component={MyPage} />
-				<Route path="/google-material" component={GoogleMaterialPage} />
-				<Route path="/react-bootstrap" component={ReactBootstrapPage} />
-				<Route path="/e-commerce" component={ECommercePage} />
-				<Redirect to="error/error-v1" />
-			</Switch>
-		</Suspense>
-	);
+  return (
+    <Suspense fallback={<LayoutSplashScreen />}>
+      <Switch>
+        {
+          /* Redirect from root URL to /dashboard. */
+          <Redirect exact from='/' to='/dashboard' />
+        }
+        <ContentRoute path='/dashboard' component={DashboardPage} />
+        <ContentRoute path='/admin-management' component={AdminManagement} />
+        <ContentRoute
+          path='/corporate-management'
+          component={CorporateManagement}
+        />
+        <ContentRoute path='/builder' component={BuilderPage} />
+        <ContentRoute path='/my-page' component={MyPage} />
+        <Route path='/google-material' component={GoogleMaterialPage} />
+        <Route path='/react-bootstrap' component={ReactBootstrapPage} />
+        <Route path='/e-commerce' component={ECommercePage} />
+        <Redirect to='error/error-v1' />
+      </Switch>
+    </Suspense>
+  );
 }
