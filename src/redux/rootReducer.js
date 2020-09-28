61227e961cf375d9e26909b80680541e7b778c34
Keyur Savaliya <keyurs8589@gmail.com>
2020-09-28 17:40:57 +1000

    add: manage corporate component

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,12 +1,13 @@
-import {all} from "redux-saga/effects";
-import {combineReducers} from "redux";
-import * as auth from "../app/modules/Auth/_redux/authRedux";
-import {customersSlice} from "../app/modules/ECommerce/_redux/customers/customersSlice";
-import {productsSlice} from "../app/modules/ECommerce/_redux/products/productsSlice";
-import {remarksSlice} from "../app/modules/ECommerce/_redux/remarks/remarksSlice";
-import {specificationsSlice} from "../app/modules/ECommerce/_redux/specifications/specificationsSlice";
+import { all } from 'redux-saga/effects';
+import { combineReducers } from 'redux';
+import * as auth from '../app/modules/Auth/_redux/authRedux';
+import { customersSlice } from '../app/modules/ECommerce/_redux/customers/customersSlice';
+import { productsSlice } from '../app/modules/ECommerce/_redux/products/productsSlice';
+import { remarksSlice } from '../app/modules/ECommerce/_redux/remarks/remarksSlice';
+import { specificationsSlice } from '../app/modules/ECommerce/_redux/specifications/specificationsSlice';
 import manageUserReducer from '../app/reducers/manageUser.reducer';
 import rolesAndPermissionReducer from '../app/reducers/rolesAndPermission.reducer';
+import manageCorporateReducer from '../app/reducers/manageCorporate.reducer';
 
 export const rootReducer = combineReducers({
   auth: auth.reducer,
@@ -14,13 +15,11 @@ export const rootReducer = combineReducers({
   products: productsSlice.reducer,
   remarks: remarksSlice.reducer,
   specifications: specificationsSlice.reducer,
-  manageUser:manageUserReducer,
-  rolesAndPermission:rolesAndPermissionReducer,
+  manageUser: manageUserReducer,
+  rolesAndPermission: rolesAndPermissionReducer,
+  manageCorporate: manageCorporateReducer,
 });
 
 export function* rootSaga() {
   yield all([auth.saga()]);
 }
-
-
-
